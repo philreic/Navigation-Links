32a0fc3c1fd33d467d71f5d43cec23023ce20229
philreic <91380439+philreic@users.noreply.github.com>
2021-10-14 17:33:16 +0200

    Initial Commit

---
+++ Navigation Links/Navigation_LinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_LinksApp.swift
+//  Navigation Links
+//
+//  Created by Philippe Reichen on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_LinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
